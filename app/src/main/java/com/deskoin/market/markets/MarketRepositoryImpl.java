797704e228adcf667cf12d2f7e0fe3c6ceacf7a8
Yoann Raucoules <y.raucoules@deskoin.com>
2022-03-08 14:36:03 +0100

    Use fixed market values instead of random values

--- app/src/main/java/com/deskoin/market/markets/MarketRepositoryImpl.java
+++ app/src/main/java/com/deskoin/market/markets/MarketRepositoryImpl.java
@@ -5,51 +5,48 @@ import com.deskoin.market.markets.model.MarketValue;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MarketRepositoryImpl implements MarketRepository {
 
-    private static final int MARKET_SIZE = 5;
-    private static final int MAX_BOUND = 10000;
-    private static final int SCALE = 2;
     private static final String KRAKEN_EXCHANGE_NAME = "kraken";
     private static final String BINANCE_EXCHANGE_NAME = "binance";
 
-    private final Random random;
-
-    public MarketRepositoryImpl(Random random) {
-        this.random = random;
-    }
-
     @Override
     public Optional<Market> findByName(String marketName) {
-        return Optional.ofNullable(getMarketStore().get(marketName));
+        return findAll().stream()
+                .filter(market -> market.getName().equalsIgnoreCase(marketName))
+                .findFirst();
     }
 
     @Override
     public List<Market> findAll() {
-        return new ArrayList<>(getMarketStore().values());
-    }
-
-    private Map<String, Market> getMarketStore() {
-        return Map.ofEntries(
-                new AbstractMap.SimpleEntry<>(KRAKEN_EXCHANGE_NAME, new Market(KRAKEN_EXCHANGE_NAME, generateValues())),
-                new AbstractMap.SimpleEntry<>(BINANCE_EXCHANGE_NAME, new Market(BINANCE_EXCHANGE_NAME, generateValues()))
+        return List.of(
+                new Market(KRAKEN_EXCHANGE_NAME, krakenValues()),
+                new Market(BINANCE_EXCHANGE_NAME, binanceValues())
         );
     }
 
-    private List<MarketValue> generateValues() {
-        ArrayList<MarketValue> marketValues = new ArrayList<>();
-        for (int i = 0; i < MARKET_SIZE; i++) {
-            marketValues.add(new MarketValue(generateValue(), generateValue()));
-        }
-        return marketValues;
+    private List<MarketValue> krakenValues() {
+        return List.of(
+                new MarketValue(new BigDecimal(10), new BigDecimal(1)),
+                new MarketValue(new BigDecimal(42), new BigDecimal(2)),
+                new MarketValue(new BigDecimal(50), new BigDecimal(10)),
+                new MarketValue(new BigDecimal(30), new BigDecimal(4)),
+                new MarketValue(new BigDecimal(90), new BigDecimal(1))
+        );
     }
 
-    private BigDecimal generateValue() {
-        return new BigDecimal(BigInteger.valueOf(random.nextInt(MAX_BOUND) + 1), SCALE);
+    private List<MarketValue> binanceValues() {
+        return List.of(
+                new MarketValue(new BigDecimal(90), new BigDecimal(8)),
+                new MarketValue(new BigDecimal(40), new BigDecimal(8)),
+                new MarketValue(new BigDecimal(100), new BigDecimal(10)),
+                new MarketValue(new BigDecimal(20), new BigDecimal(10)),
+                new MarketValue(new BigDecimal(55), new BigDecimal(1))
+        );
     }
 
 }
--- app/src/test/java/com/deskoin/market/markets/MarketRepositoryImplTest.java
+++ app/src/test/java/com/deskoin/market/markets/MarketRepositoryImplTest.java
@@ -1,6 +1,7 @@
 package com.deskoin.market.markets;
 
 import com.deskoin.market.markets.model.Market;
+import com.deskoin.market.markets.model.MarketValue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,25 +20,46 @@ import java.util.Random;
 @ExtendWith(MockitoExtension.class)
 class MarketRepositoryImplTest {
 
-    @Mock
-    private Random random;
-
     @InjectMocks
     private MarketRepositoryImpl marketRepositoryImpl;
 
     @Test
     void findAll() {
-        Mockito.when(random.nextInt(Mockito.eq(10000))).thenReturn(100);
         List<Market> markets = marketRepositoryImpl.findAll();
+
         Assertions.assertEquals(2, markets.size());
-        Assertions.assertNotNull(markets.get(0).getName());
-        Assertions.assertNotNull(markets.get(1).getName());
-        Assertions.assertEquals(5, markets.get(0).getValues().size());
-        Assertions.assertEquals(BigDecimal.valueOf(1.01), markets.get(0).getValues().get(0).getPrice());
-        Assertions.assertEquals(BigDecimal.valueOf(1.01), markets.get(0).getValues().get(0).getVolume());
-        Assertions.assertEquals(5, markets.get(1).getValues().size());
-        Assertions.assertEquals(BigDecimal.valueOf(1.01), markets.get(1).getValues().get(0).getPrice());
-        Assertions.assertEquals(BigDecimal.valueOf(1.01), markets.get(1).getValues().get(0).getVolume());
+
+        var krakenMarket = markets.get(0);
+        Assertions.assertNotNull(krakenMarket);
+        Assertions.assertEquals("kraken", krakenMarket.getName());
+        var krakenMarketValues = krakenMarket.getValues();
+        Assertions.assertEquals(5, krakenMarketValues.size());
+        Assertions.assertEquals(new BigDecimal(10), krakenMarketValues.get(0).getPrice());
+        Assertions.assertEquals(new BigDecimal(1), krakenMarketValues.get(0).getVolume());
+        Assertions.assertEquals(new BigDecimal(42), krakenMarketValues.get(1).getPrice());
+        Assertions.assertEquals(new BigDecimal(2), krakenMarketValues.get(1).getVolume());
+        Assertions.assertEquals(new BigDecimal(50), krakenMarketValues.get(2).getPrice());
+        Assertions.assertEquals(new BigDecimal(10), krakenMarketValues.get(2).getVolume());
+        Assertions.assertEquals(new BigDecimal(30), krakenMarketValues.get(3).getPrice());
+        Assertions.assertEquals(new BigDecimal(4), krakenMarketValues.get(3).getVolume());
+        Assertions.assertEquals(new BigDecimal(90), krakenMarketValues.get(4).getPrice());
+        Assertions.assertEquals(new BigDecimal(1), krakenMarketValues.get(4).getVolume());
+
+        var binanceMarket = markets.get(1);
+        Assertions.assertNotNull(binanceMarket);
+        Assertions.assertEquals("binance", binanceMarket.getName());
+        var binanceMarketValues = binanceMarket.getValues();
+        Assertions.assertEquals(5, binanceMarketValues.size());
+        Assertions.assertEquals(new BigDecimal(90), binanceMarketValues.get(0).getPrice());
+        Assertions.assertEquals(new BigDecimal(8), binanceMarketValues.get(0).getVolume());
+        Assertions.assertEquals(new BigDecimal(40), binanceMarketValues.get(1).getPrice());
+        Assertions.assertEquals(new BigDecimal(8), binanceMarketValues.get(1).getVolume());
+        Assertions.assertEquals(new BigDecimal(100), binanceMarketValues.get(2).getPrice());
+        Assertions.assertEquals(new BigDecimal(10), binanceMarketValues.get(2).getVolume());
+        Assertions.assertEquals(new BigDecimal(20), binanceMarketValues.get(3).getPrice());
+        Assertions.assertEquals(new BigDecimal(10), binanceMarketValues.get(3).getVolume());
+        Assertions.assertEquals(new BigDecimal(55), binanceMarketValues.get(4).getPrice());
+        Assertions.assertEquals(new BigDecimal(1), binanceMarketValues.get(4).getVolume());
     }
 
     @ParameterizedTest
@@ -46,8 +68,8 @@ class MarketRepositoryImplTest {
             "binance"
     })
     void findAll(String marketName) {
-        Mockito.when(random.nextInt(Mockito.eq(10000))).thenReturn(100);
         Optional<Market> market = marketRepositoryImpl.findByName(marketName);
+
         Assertions.assertTrue(market.isPresent());
         Assertions.assertEquals(marketName, market.get().getName());
         Assertions.assertEquals(5, market.get().getValues().size());
@@ -55,8 +77,8 @@ class MarketRepositoryImplTest {
 
     @Test
     void findAll_empty() {
-        Mockito.when(random.nextInt(Mockito.eq(10000))).thenReturn(100);
         Optional<Market> market = marketRepositoryImpl.findByName("other");
+
         Assertions.assertTrue(market.isEmpty());
     }
 
